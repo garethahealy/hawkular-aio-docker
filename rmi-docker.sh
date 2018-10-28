6a6ef4ee813807665bf53dc5942e1c1498688044
Gareth Healy <garethahealy@gmail.com>
2018-10-28 14:59:30 +0000

    Updated to prune

--- rmi-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Delete all exited containers
-docker rm -f $(docker ps -a | grep Exited | awk '{print $1}')
-docker rm -f $(docker ps -a | grep Dead | awk '{print $1}')
-
-# remove dangling images (untageed)
-docker rmi $(docker images -qaf 'dangling=true')
